05030e3d324638efd778f756bd33bfa2a91f393d
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2025-04-23 15:57:50 -0400

    ✨ betadiversity plot

--- scripts/03_figures.jl
+++ scripts/03_figures.jl
@@ -6,6 +6,7 @@ using CairoMakie
 using CSV
 using DataFramesMeta
 using DrWatson
+using Statistics
 
 # Load results
 param_grid = CSV.read(datadir("param_grid.csv"), DataFrame)
@@ -35,7 +36,7 @@ renamed = Dict(
 )
 @rtransform!(param_stack, :variable = renamed[:variable])
 
-## Plot results
+## Proportion results
 
 # Common plot
 fig = data(param_stack) *
@@ -74,3 +75,43 @@ fig = data(param_stack) *
         figure=(; size=(700,450))
     )
 save(plotsdir("nbon_logx.png"), fig; px_per_unit=2.0)
+
+## Betadiversity results
+
+# Load results
+beta_res = CSV.read(datadir("betadiversity.csv"), DataFrame)
+filter!(:refmethod => ==("global"), beta_res)
+
+# Summarize
+# beta_gp = @chain begin
+#     beta_res
+#     groupby([:nbon, :nrep])
+#     @combine(
+#         :βos_possible = median(:βos_possible),
+#         :βos_realized = median(:βos_realized),
+#         :βos_detected = median(:βos_detected),
+#     )
+# end
+@transform!(beta_res, :monitored_sp = NaN)
+
+# Plot
+fig = data(beta_res) *
+    visual(
+        RainClouds;
+        markersize=4, jitter_width=0.0, clouds=nothing, plot_boxplots=false
+    ) *
+    mapping(
+        :nbon => "Number of sites in BON",
+        [:monitored_sp, :βos_possible, :βos_realized, :βos_detected] .=> "βos' (monitored vs real metaweb)";
+        color=dims(1) =>
+            renamer(["Monitored sp", "Possible int", "Realized int", "Detected int"]) =>
+            "Sampled Element",
+        # color=:variable => presorted => "Sampled element",
+        # layout=:refmethod => renamer("global" => "Global reference", "metawebify" => "Per-element reference"),
+    ) |> x ->
+    draw(x,
+        axis=(; yticks=(0.0:0.25:1.0), xticks=(0:25:100)),
+        legend=(; framevisible=false),
+        # figure=(; size=(700,450))
+    )
+save(plotsdir("betadiversity.png"), fig; px_per_unit=2.0)
