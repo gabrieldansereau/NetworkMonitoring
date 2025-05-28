99da94ee4c7ae42d0909e373ea4fbb6b98473e58
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2025-05-28 11:54:55 -0400

    🎨 add optimization layers as insets on figures

--- scripts/05_focal_plots.jl
+++ scripts/05_focal_plots.jl
@@ -16,6 +16,11 @@ monitored_optimized = CSV.read(datadir("monitored_optimized.csv"), DataFrame)
 sp = monitored_types.sp[1]
 deg = maximum(monitored_types.deg)
 
+# Load layers used for optimization
+focal_sp_range = SDT.SDMLayer(datadir("layer_sp_range.tiff"))
+richness_spp = SDT.SDMLayer(datadir("layer_richness_spp.tiff"))
+degree_realized = SDT.SDMLayer(datadir("layer_degree_realized.tiff"))
+
 ## Define labels and colors for all plots
 
 # Colors
@@ -113,36 +118,134 @@ save(plotsdir("focal_spp.png"), fig)
 
 ## Explore variations with different sampler
 
+# Generate BON examples
+begin
+    Random.seed!(42)
+    bons = Dict()
+    bons["UncertaintySampling"] = BON.sample(BON.UncertaintySampling(100), focal_sp_range)
+    bons["WeightedBalancedAcceptance"] = BON.sample(
+        BON.WeightedBalancedAcceptance(100), focal_sp_range
+    )
+    bons["SimpleRandom"] = BON.sample(BON.SimpleRandom(100), focal_sp_range)
+end
+
 # Plot
 begin
     res = monitored_samplers
     fig = Figure()
-    ax = Axis(fig[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions")
+    # Create layouts
+    ga = GridLayout(fig[:, 1:3])
+    gb = GridLayout(fig[:, end + 1])
+    # Create axes
+    ax = Axis(
+        ga[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions", xticks=0:100:500
+    )
+    ax1 = Axis(
+        gb[1, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax2 = Axis(
+        gb[2, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax3 = Axis(
+        gb[3, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    # Remove decorations for heatmaps
+    hidedecorations!(ax1; label=false)
+    hidedecorations!(ax2; label=false)
+    hidedecorations!(ax3; label=false)
+    # Sampling results
     for s in unique(res.sampler)
         b = filter(:sampler => ==(s), res)
-        band!(b.nbon, b.low, b.upp; alpha=0.4, label=s, color=cols[s])
-        lines!(b.nbon, b.med; label=s, color=cols[s])
+        band!(ax, b.nbon, b.low, b.upp; alpha=0.4, label=s, color=cols[s])
+        lines!(ax, b.nbon, b.med; label=s, color=cols[s])
     end
     hlines!(ax, deg; linestyle=:dash, alpha=0.5, color=:grey, label="metaweb")
-    axislegend(; position=:lt, merge=true)
+    axislegend(ax; position=:lt, merge=true, labelsize=14)
+    # Legend(ga[2,1], ax, orientation=:horizontal, merge=true, nbanks=2)
+    # Heatmaps & BON example
+    for (a, s) in zip([ax1, ax2, ax3], unique(res.sampler))
+        heatmap!(a, focal_sp_range)
+        scatter!(a, coordinates(bons[s]); markersize=5, color=cols[s], strokewidth=0.5)
+        a.ylabel = s
+    end
+    # Subpanel labels
+    Label(ga[1, :, Top()], "Sampler efficiency"; padding=(0, 0, 5, 0), font=:bold)
+    Label(gb[1, :, Top()], "BON examples"; padding=(0, 0, 5, 0), font=:bold)
+    # Show figure
     fig
 end
 save(plotsdir("focal_samplers.png"), fig)
 
 ## Optimized sampling
 
+# Generate BON examples
+begin
+    Random.seed!(33)
+    bons["Focal species range"] = bons["UncertaintySampling"]
+    bons["Species richness"] = BON.sample(BON.UncertaintySampling(100), richness_spp)
+    bons["Realized interactions"] = BON.sample(
+        BON.UncertaintySampling(100), degree_realized
+    )
+end
+
+# Collect layers
+begin
+    layers = Dict()
+    layers["Focal species range"] = focal_sp_range
+    layers["Species richness"] = richness_spp
+    layers["Realized interactions"] = degree_realized
+end
+
+# Reorder elements for display
+_order = Dict(
+    "Realized interactions" => 1, "Focal species range" => 2, "Species richness" => 3
+)
+sort!(monitored_optimized, order(:sampler; by=x -> _order[x]))
+
 # Plot
 begin
     res = monitored_optimized
     fig = Figure()
-    ax = Axis(fig[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions")
+    # Create layouts
+    ga = GridLayout(fig[:, 1:3])
+    gb = GridLayout(fig[:, end + 1])
+    # Create axes
+    ax = Axis(
+        ga[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions", xticks=0:100:500
+    )
+    ax1 = Axis(
+        gb[1, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax2 = Axis(
+        gb[2, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax3 = Axis(
+        gb[3, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    # Remove decorations for heatmaps
+    hidedecorations!(ax1; label=false)
+    hidedecorations!(ax2; label=false)
+    hidedecorations!(ax3; label=false)
+    # Sampling results
     for s in unique(res.sampler)
         b = filter(:sampler => ==(s), res)
-        band!(b.nbon, b.low, b.upp; alpha=0.4, label=s, color=cols[s])
-        lines!(b.nbon, b.med; label=s, color=cols[s])
+        band!(ax, b.nbon, b.low, b.upp; alpha=0.4, label=s, color=cols[s])
+        lines!(ax, b.nbon, b.med; label=s, color=cols[s])
     end
     hlines!(ax, deg; linestyle=:dash, alpha=0.5, color=:grey, label="metaweb")
-    axislegend("Optimized on"; position=:lt, merge=true)
+    axislegend(ax; position=:lt, merge=true, labelsize=14)
+    # Heatmaps & BON example
+    for (a, s) in zip([ax1, ax2, ax3], unique(res.sampler))
+        heatmap!(a, layers[s])
+        scatter!(a, coordinates(bons[s]); markersize=5, color=cols[s], strokewidth=0.5)
+        a.ylabel = s
+    end
+    # Subpanel labels
+    Label(
+        ga[1, :, Top()], "Optimization layer efficiency"; padding=(0, 0, 5, 0), font=:bold
+    )
+    Label(gb[1, :, Top()], "BON examples"; padding=(0, 0, 5, 0), font=:bold)
+    # Show figure
     fig
 end
 save(plotsdir("focal_optimized.png"), fig)
