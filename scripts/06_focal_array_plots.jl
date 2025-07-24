999c69d862e5a79719c69f199865582f02d248e3
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2025-07-24 15:03:09 -0400

    🚧 double median all the way across the sky 🌤️ 🌈

--- scripts/06_focal_array_plots.jl
+++ scripts/06_focal_array_plots.jl
@@ -71,12 +71,24 @@ sims_optimized
 monitored_samplers = @chain sims_samplers begin
     @rtransform(:lowdiff = :med - :low, :uppdiff = :upp - :med)
     groupby([:sampler, :nbon])
-    @combine(:low = mean(:lowdiff), :med = median(:med), :upp = mean(:uppdiff))
+    @combine(
+        :low = mean(:lowdiff),
+        :med = median(:med),
+        :upp = mean(:uppdiff),
+        :lowmed = median(:lowdiff),
+        :uppmed = median(:uppdiff),
+    )
 end
 monitored_optimized = @chain sims_optimized begin
     @rtransform(:lowdiff = :med - :low, :uppdiff = :upp - :med)
     groupby([:sampler, :nbon])
-    @combine(:low = mean(:lowdiff), :med = median(:med), :upp = mean(:uppdiff),)
+    @combine(
+        :low = mean(:lowdiff),
+        :med = median(:med),
+        :upp = mean(:uppdiff),
+        :lowmed = median(:lowdiff),
+        :uppmed = median(:uppdiff),
+    )
 end
 
 ## Define labels and colors for all plots
@@ -256,6 +268,60 @@ begin
 end
 save(plotsdir("focal_array_samplers_med_mean.png"), fig)
 
+begin
+    res = monitored_samplers
+    fig = Figure()
+    # Create layouts
+    ga = GridLayout(fig[:, 1:3])
+    gb = GridLayout(fig[:, end + 1])
+    # Create axes
+    ax = Axis(
+        ga[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions", xticks=0:100:500
+    )
+    ax1 = Axis(
+        gb[1, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax2 = Axis(
+        gb[2, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax3 = Axis(
+        gb[3, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    # Remove decorations for heatmaps
+    hidedecorations!(ax1; label=false)
+    hidedecorations!(ax2; label=false)
+    hidedecorations!(ax3; label=false)
+    # Sampling results
+    for s in unique(res.sampler)
+        b = filter(:sampler => ==(s), res)
+        band!(
+            ax,
+            b.nbon,
+            b.med .- b.lowmed,
+            b.med .+ b.uppmed;
+            alpha=0.4,
+            label=s,
+            color=cols[s],
+        )
+        lines!(ax, b.nbon, b.med; label=s, color=cols[s])
+    end
+    hlines!(ax, [1.0]; linestyle=:dash, alpha=0.5, color=:grey, label="metaweb")
+    axislegend(ax; position=:lt, merge=true, labelsize=14)
+    # Legend(ga[2,1], ax, orientation=:horizontal, merge=true, nbanks=2)
+    # Heatmaps & BON example
+    for (a, s) in zip([ax1, ax2, ax3], unique(res.sampler))
+        heatmap!(a, focal_sp_range)
+        scatter!(a, coordinates(bons[s]); markersize=5, color=cols[s], strokewidth=0.5)
+        a.ylabel = s
+    end
+    # Subpanel labels
+    Label(ga[1, :, Top()], "Sampler efficiency"; padding=(0, 0, 5, 0), font=:bold)
+    Label(gb[1, :, Top()], "BON examples"; padding=(0, 0, 5, 0), font=:bold)
+    # Show figure
+    fig
+end
+save(plotsdir("focal_array_samplers_med_med.png"), fig)
+
 ## Optimized sampling
 
 # Generate BON examples
@@ -332,6 +398,7 @@ begin
     fig
 end
 save(plotsdir("focal_array_optimized_one.png"), fig)
+
 begin
     res = monitored_optimized
     fig = Figure()
@@ -378,3 +445,58 @@ begin
     fig
 end
 save(plotsdir("focal_array_optimized_med_mean.png"), fig)
+
+begin
+    res = monitored_optimized
+    fig = Figure()
+    # Create layouts
+    ga = GridLayout(fig[:, 1:3])
+    gb = GridLayout(fig[:, end + 1])
+    # Create axes
+    ax = Axis(
+        ga[1, 1]; xlabel="Sites in BON", ylabel="Monitored interactions", xticks=0:100:500
+    )
+    ax1 = Axis(
+        gb[1, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax2 = Axis(
+        gb[2, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    ax3 = Axis(
+        gb[3, 1]; aspect=1, yaxisposition=:right, ylabelrotation=1.5pi, ylabelsize=10
+    )
+    # Remove decorations for heatmaps
+    hidedecorations!(ax1; label=false)
+    hidedecorations!(ax2; label=false)
+    hidedecorations!(ax3; label=false)
+    # Sampling results
+    for s in unique(res.sampler)
+        b = filter(:sampler => ==(s), res)
+        band!(
+            ax,
+            b.nbon,
+            b.med - b.lowmed,
+            b.med + b.uppmed;
+            alpha=0.4,
+            label=s,
+            color=cols[s],
+        )
+        lines!(ax, b.nbon, b.med; label=s, color=cols[s])
+    end
+    hlines!(ax, [1.0]; linestyle=:dash, alpha=0.5, color=:grey, label="metaweb")
+    axislegend(ax; position=:lt, merge=true, labelsize=14)
+    # Heatmaps & BON example
+    for (a, s) in zip([ax1, ax2, ax3], unique(res.sampler))
+        heatmap!(a, layers[s])
+        scatter!(a, coordinates(bons[s]); markersize=5, color=cols[s], strokewidth=0.5)
+        a.ylabel = s
+    end
+    # Subpanel labels
+    Label(
+        ga[1, :, Top()], "Optimization layer efficiency"; padding=(0, 0, 5, 0), font=:bold
+    )
+    Label(gb[1, :, Top()], "BON examples"; padding=(0, 0, 5, 0), font=:bold)
+    # Show figure
+    fig
+end
+save(plotsdir("focal_array_optimized_med_med.png"), fig)
