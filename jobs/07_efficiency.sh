dc0cfe7c711129474f341bdddcec4f70b6cc4326
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2025-08-18 11:48:49 -0400

    🚧 temporarily comment out code for other options

--- jobs/07_efficiency.sh
+++ jobs/07_efficiency.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=ctb-tpoisot
-#SBATCH --array=1-100
+#SBATCH --array=1-5
 #SBATCH --ntasks=1
 #SBATCH --mem=4G
-#SBATCH --time=01:15:00
+#SBATCH --time=01:30:00
 #SBATCH --job-name=04_focal_array
 #SBATCH --output=jobs/out/%x-%J.out
 
@@ -13,4 +13,4 @@ cd $HOME/projects/def-tpoisot/gabdans/NetworkMonitoring
 
 export PROGRESS_BARS_DT=60
 
-julia --project -e 'const NREP = 50; const OUTDIR = "efficiency"; include("scripts/04_focal_sims.jl")'
+julia --project -e 'const NREP = 50; const OUTDIR = "efficiency"; repid = 0; for i in 1:20; global repid +=1; include("scripts/04_focal_sims.jl"); end'
