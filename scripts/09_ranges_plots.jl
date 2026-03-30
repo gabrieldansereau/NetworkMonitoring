f0a1669501c7f21783f38802db4dd7e8cb66245e
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2026-03-30 14:48:15 -0400

    🚚 move range efficiency distributions to supp

--- scripts/09_ranges_plots.jl
+++ scripts/09_ranges_plots.jl
@@ -7,93 +7,6 @@ include("include.jl") # see note regarding why we cannot use the module
 # Load data
 effs_estimations = CSV.read(datadir("efficiency_estimations.csv"), DataFrame)
 
-# Sort values
-sorted = unique(filter(:sim => ==(1), effs_estimations).variable)
-
-# Median
-med = median(filter(:variable => startswith("True"), effs_estimations).eff)
-
-# Palette
-palette = Dict()
-for v in sorted
-    if startswith("Over")(v)
-        col = "1"
-    elseif startswith("Under")(v)
-        col = "2"
-    else
-        col = "3"
-    end
-    palette[v] = col
-end
-
-# Plot all efficiencies
-begin
-    Random.seed!(42) # for jitter
-    eff = :eff => "efficiency"
-    layer =
-        mapping(
-            :variable => sorter(sorted) => "", eff; color=:variable => (x -> palette[x])
-        ) * visual(
-            RainClouds;
-            markersize=5,
-            jitter_width=0.1,
-            plot_boxplots=false,
-            orientation=:horizontal,
-        )
-    vline = mapping([med]) * visual(VLines; linestyle=:dash)
-    f1 = data(effs_estimations) * layer + vline
-    f = Figure(; size=(700, 800))
-    # sc = scales(; Color=(; palette=palette))
-    ylog2 = (; axis=(;))
-    fg1 = draw!(f[1, 1], f1; ylog2...)
-    pad = (-45, 0, 20, 0)
-    Label(f[1, 1, Top()], "Range estimations"; halign=:left, font=:bold, padding=pad)
-    save(plotsdir("ranges_efficiencies_all.png"), f)
-    f
-end
-
-# Plot subset
-begin
-    set = [
-        "Under-0.50",
-        "Under-0.40",
-        "Under-0.30",
-        "Under-0.20",
-        "Under-0.10",
-        "True-0.00",
-        "Over-0.10",
-        "Over-0.20",
-        "Over-0.30",
-        "Over-0.40",
-        "Over-0.50",
-    ]
-    res = filter(:variable => in(set), effs_estimations)
-    Random.seed!(42) # for jitter
-    eff = :eff => "efficiency"
-    layer =
-        mapping(
-            :variable => sorter(sorted) => "", eff; color=:variable => (x -> palette[x])
-        ) * visual(
-            RainClouds;
-            markersize=5,
-            jitter_width=0.1,
-            # violin_limits=extrema,
-            plot_boxplots=false,
-            orientation=:horizontal,
-            gap=0.01,
-        )
-    vline = mapping([med]) * visual(VLines; linestyle=:dash)
-    f1 = data(res) * layer + vline
-    f = Figure(;)
-    # sc = scales(; Color=(; palette=palette))
-    ylog2 = (; axis=(;))
-    fg1 = draw!(f[1, 1], f1; ylog2...)
-    pad = (-45, 0, 20, 0)
-    Label(f[1, 1, Top()], "Range estimations"; halign=:left, font=:bold, padding=pad)
-    save(plotsdir("ranges_efficiencies.png"), f)
-    f
-end
-
 ## Fill-in all possible offset values for simulations with missing results
 
 # Check the proportion of simulations with results
@@ -515,3 +428,95 @@ begin
     save(plotsdir("ranges_confidence_interval.png"), current_figure())
     f
 end
+
+## Check the distribution of range efficiencies
+#=
+
+# Sort values
+sorted = unique(filter(:sim => ==(1), effs_estimations).variable)
+
+# Median
+med = median(filter(:variable => startswith("True"), effs_estimations).eff)
+
+# Palette
+palette = Dict()
+for v in sorted
+    if startswith("Over")(v)
+        col = "1"
+    elseif startswith("Under")(v)
+        col = "2"
+    else
+        col = "3"
+    end
+    palette[v] = col
+end
+
+# Plot all efficiencies
+begin
+    Random.seed!(42) # for jitter
+    eff = :eff => "efficiency"
+    layer =
+        mapping(
+            :variable => sorter(sorted) => "", eff; color=:variable => (x -> palette[x])
+        ) * visual(
+            RainClouds;
+            markersize=5,
+            jitter_width=0.1,
+            plot_boxplots=false,
+            orientation=:horizontal,
+        )
+    vline = mapping([med]) * visual(VLines; linestyle=:dash)
+    f1 = data(effs_estimations) * layer + vline
+    f = Figure(; size=(700, 800))
+    # sc = scales(; Color=(; palette=palette))
+    ylog2 = (; axis=(;))
+    fg1 = draw!(f[1, 1], f1; ylog2...)
+    pad = (-45, 0, 20, 0)
+    Label(f[1, 1, Top()], "Range estimations"; halign=:left, font=:bold, padding=pad)
+    save(plotsdir("supp", "ranges_efficiencies_all.png"), f)
+    f
+end
+
+# Plot subset
+begin
+    set = [
+        "Under-0.50",
+        "Under-0.40",
+        "Under-0.30",
+        "Under-0.20",
+        "Under-0.10",
+        "True-0.00",
+        "Over-0.10",
+        "Over-0.20",
+        "Over-0.30",
+        "Over-0.40",
+        "Over-0.50",
+    ]
+    res = filter(:variable => in(set), effs_estimations)
+    Random.seed!(42) # for jitter
+    eff = :eff => "efficiency"
+    layer =
+        mapping(
+            :variable => sorter(sorted) => "", eff; color=:variable => (x -> palette[x])
+        ) * visual(
+            RainClouds;
+            markersize=5,
+            jitter_width=0.1,
+            # violin_limits=extrema,
+            plot_boxplots=false,
+            orientation=:horizontal,
+            gap=0.01,
+        )
+    vline = mapping([med]) * visual(VLines; linestyle=:dash)
+    f1 = data(res) * layer + vline
+    f = Figure(;)
+    # sc = scales(; Color=(; palette=palette))
+    ylog2 = (; axis=(;))
+    fg1 = draw!(f[1, 1], f1; ylog2...)
+    pad = (-45, 0, 20, 0)
+    Label(f[1, 1, Top()], "Range estimations"; halign=:left, font=:bold, padding=pad)
+    save(plotsdir("supp", "ranges_efficiencies.png"), f)
+    f
+end
+
+=#
