cb8c4897998e09dc7a0623826ba5041119f9522b
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2026-04-15 18:50:17 -0400

    Revert "🕵️ check efficiency with pmax=1.0 and p at n=500"
    This reverts commit 1fcb0582eb35f25ac4010e3f82e8b5b384724934.

--- scripts/07_efficiency.jl
+++ scripts/07_efficiency.jl
@@ -136,9 +136,9 @@ effs_estimations = DataFrame()
     occ = occup[sim]
     set = first(gd.set)
     # Compute efficiency
-    eff = efficiency(gd.nbon, gd.med; f=exp, pmax=1.0, option=:p_at_n, n=500)
-    eff_low = efficiency(gd.nbon, gd.confint_low; f=exp, pmax=1.0, option=:p_at_n, n=500)
-    eff_upp = efficiency(gd.nbon, gd.confint_upp; f=exp, pmax=1.0, option=:p_at_n, n=500)
+    eff = efficiency(gd.nbon, gd.med; f=exp, pmax=1.0)
+    eff_low = efficiency(gd.nbon, gd.confint_low; f=exp, pmax=1.0)
+    eff_upp = efficiency(gd.nbon, gd.confint_upp; f=exp, pmax=1.0)
     # Export
     row = (;
         sim=sim,
