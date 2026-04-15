9c30ef7f84fe3b38d438afe56ac2d851af380009
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2026-04-15 18:38:51 -0400

    🚫 tried efficiency with n at p=0.80
    Figures are produced, but with missing axes and data. Might be because p=0.80 is too high in many overestimation scenarios

--- scripts/07_efficiency.jl
+++ scripts/07_efficiency.jl
@@ -136,9 +136,9 @@ effs_estimations = DataFrame()
     occ = occup[sim]
     set = first(gd.set)
     # Compute efficiency
-    eff = efficiency(gd.nbon, gd.med; f=exp, pmax=1.0, option=:integral)
-    eff_low = efficiency(gd.nbon, gd.confint_low; f=exp, pmax=1.0, option=:integral)
-    eff_upp = efficiency(gd.nbon, gd.confint_upp; f=exp, pmax=1.0, option=:integral)
+    eff = efficiency(gd.nbon, gd.med; f=exp, pmax=pmax, option=:n_at_p, p=0.80)
+    eff_low = efficiency(gd.nbon, gd.confint_low; f=exp, pmax=pmax, option=:n_at_p, p=0.80)
+    eff_upp = efficiency(gd.nbon, gd.confint_upp; f=exp, pmax=pmax, option=:n_at_p, p=0.80)
     # Export
     row = (;
         sim=sim,
